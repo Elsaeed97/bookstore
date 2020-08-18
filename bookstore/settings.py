b721ccb88d98b1d1ade30691b8f1ccdf0b643b1b
Elsaeed Ahmed <elsaeedahmed97@gmail.com>
2020-08-18 18:41:12 +0200

    Create some user functionality

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,10 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party 
+    'crispy_forms', 
+
     # local apps
     'users.apps.UsersConfig', 
     'pages.apps.PagesConfig',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4' 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -128,8 +132,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 
 
