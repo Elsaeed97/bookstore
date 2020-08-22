60543310e15b36ade32c0a122be3c2714a2ad338
Elsaeed Ahmed <elsaeedahmed97@gmail.com>
2020-08-22 16:15:51 +0200

    Add Custom Permissions

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import book_list,book_detail
+from .views import book_list,BookDetailView
 urlpatterns =[
-	path('<uuid:pk>/', book_detail, name='book_detail'),
+	path('<uuid:pk>/', BookDetailView.as_view() , name='book_detail'),
 	path('', book_list, name='book_list' ),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
+from django.views.generic import ListView, DetailView
+
+
 # Create your views here.
 
+@login_required
 def book_list(request):
 	books = Book.objects.all()
 	context ={
@@ -9,10 +15,18 @@ def book_list(request):
 	}
 	return render(request, 'books/book_list.html',context)
 
-def book_detail(request, pk):
-	book = Book.objects.get(id=pk)
-	context = {
-		'book':book,
+# @login_required
+# def book_detail(request, pk):
+# 	book = Book.objects.get(id=pk)
+# 	context = {
+# 		'book':book,
 
-	}
-	return render(request,'books/book_detail.html', context)
+# 	}
+# 	return render(request,'books/book_detail.html', context)
+
+class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin, DetailView):
+	model = Book
+	context_object_name = 'book'
+	template_name = 'books/book_detail.html'
+	login_url = 'account_login'
+	permission_required = 'books.special_status' 
